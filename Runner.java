dd6b33666403cc65c5b14f7a5a699715dff83506
Ashutosh Mishra <35286920+nityagautam@users.noreply.github.com>
2019-08-27 20:30:38 +0530

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,36 @@
+package nng;
+
+import org.openqa.selenium.By;
+
+import nng.automator.ui.Main;
+import nng.utils.Browser;
+
+public class Runner{
+
+	public static void main(String[] args) {
+		// GUI AUTOMATION
+		//Main w = new Main();
+		//w.startFrame();
+		
+		// AUTOMATION
+		String pgUrl = "https://wikipedia.com"; 
+		int maxWaitTime = 10;
+		String browser = "CHROME";
+		//String driverLocation = "C:\\_DevOpsTools\\WebDriver\\chromedriver.exe";
+		String driverLocation = "/Users/nityagautam/WorkSpace/WebDrivers/chrome/chromedriver";
+		Browser b = new Browser(pgUrl, maxWaitTime, browser, driverLocation);
+		
+		// Start
+		b.initialize().
+			open().
+			type(By.xpath("//*[@id='searchInput']"), "Earth").
+			click(By.xpath("//*[text()='Search']/.."));
+		
+		// Close driver
+		b.close();
+		
+		// Quit
+		b.getDriver().quit();
+	}
+
+}
